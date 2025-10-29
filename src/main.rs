a769438474befd7f7e9dce4bd957b69696e4c0b2
Oleg Moshenskiy <o.moshenskiy@ukma.edu.ua>
2025-10-29 14:56:52 +0200

    work on a calc func

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     println!("Enter a number to calculate: ");
     let mut input = String::new();
     io::stdin().read_line(&mut input);
-    let num = parse_into_a_number(&input);
+    let num = parse_number(&input);
     let sign = parse_sign(&input);
     let res = calc(num, sign);
     println!("Result: {:?}", res);
 }
 
-fn parse_into_a_number(input: &String) -> Vec<i32> {
+fn parse_number(input: &String) -> Vec<i32> {
     let mut number: Vec<i32> = input
         .replace("+", " ")
         .replace("-", " ")
@@ -38,15 +38,26 @@ fn parse_sign(input: &String) -> Vec<char> {
 }
 
 fn calc(number: Vec<i32>, ch: Vec<char>) -> i32 {
+    let mut index: Vec<usize> = Vec::new();
     let mut res = number[0];
+    let mut mult_div = number[0];
     for a in 0..number.len() - 1 {
         let mut num2 = number[a + 1];
-        res = match ch[a] {
-            '+' => res + num2,
-            '-' => res - num2,
-            '*' => res * num2,
-            '/' => res / num2,
-            _ => 0,
+
+        for b in 0..ch.len() {
+            if ch[b] == '*' || ch[b] == '/' {
+                index.push(b);
+            }
+        }
+        if index.contains(&a) {
+            if ch[a] == '*' {
+                mult_div *= num2;
+            }
+            if ch[a] == '/' {
+                mult_div /= num2;
+            }
+        } else {
+            res += if ch[a] == '+' { mult_div } else { -mult_div }
         }
     }
     return res;
