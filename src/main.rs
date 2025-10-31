f4bb6d6f403e706032d49dcf6aa6df0dda1da201
Oleg Moshenskiy <o.moshenskiy@ukma.edu.ua>
2025-10-31 20:30:34 +0200

    complete functioncs

--- src/main.rs
+++ src/main.rs
@@ -6,74 +6,47 @@ fn main() {
     println!("Enter a number to calculate: ");
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    check_bracket(&input);
+    check_bracket(&input.trim().to_string());
 }
 
 fn check_bracket(input: &String) -> i32 {
-    let mut is_bracket = false;
     let mut res: i32 = 0;
-
-    if input.contains("(") && input.contains(")") {
-        is_bracket = true;
-    } else {
-        is_bracket = false;
-    }
-
-    if is_bracket {
+    let mut new_input = input.clone();
+    while new_input.contains('(') && new_input.contains(')') {
         let mut value_bracket: i32 = 0;
         let mut num_in_bracket: Vec<i32> = Vec::new();
         let mut sign_in_bracket: Vec<char> = Vec::new();
         let mut in_bracket: String = String::new();
-        let mut bracket_position: Vec<usize> = Vec::new();
-        let input_chars: Vec<char> = input.chars().collect();
-        let mut c = false;
+        let input_chars: Vec<char> = new_input.chars().collect();
 
-        for a in 0..input_chars.len() {
-            if input_chars[a] == '(' {
-                c = true;
-                bracket_position.push(a);
-                continue;
-            } else if input_chars[a] == ')'
-                && input_chars[a + 1] != '+'
-                && input_chars[a + 1] != '-'
-                && input_chars[a + 1] != '*'
-                && input_chars[a + 1] != '/'
-            {
-                c = false;
-                bracket_position.push(a);
+        let mut open_bracket: Option<usize> = None;
+        let mut close_bracket: Option<usize> = None;
+
+        for (a, &c) in input_chars.iter().enumerate() {
+            if c == '(' {
+                open_bracket = Some(a);
+            } else if c == ')' && open_bracket.is_some() {
+                close_bracket = Some(a);
                 break;
-            } else if input_chars[a] == ')'
-                || input_chars[a + 1] == '+'
-                || input_chars[a + 1] == '-'
-                || input_chars[a + 1] == '*'
-                || input_chars[a + 1] == '/'
-            {
-                c = true;
-                continue;
-            }
-            if c {
-                in_bracket.push(input_chars[a]);
             }
         }
+
+        let index_start = open_bracket.unwrap();
+        let index_end = close_bracket.unwrap();
+        in_bracket = input_chars[index_start + 1..index_end].iter().collect();
+
         num_in_bracket = parse_number(&in_bracket);
         sign_in_bracket = parse_sign(&in_bracket);
         value_bracket = calc(num_in_bracket, sign_in_bracket);
 
-        let index_start = bracket_position[0];
-        let index_end = bracket_position[bracket_position.len() - 1];
-        let mut new_input = input.clone();
-
         new_input.replace_range(index_start..=index_end, &value_bracket.to_string());
-
-        let number_input: Vec<i32> = parse_number(&new_input);
-        let sign_input: Vec<char> = parse_sign(&new_input);
-
-        res = calc(number_input, sign_input);
-    } else {
-        let number_input: Vec<i32> = parse_number(input);
-        let number_sign: Vec<char> = parse_sign(input);
-        res = calc(number_input, number_sign);
     }
+    let number_input: Vec<i32> = parse_number(&new_input);
+    let sign_input: Vec<char> = parse_sign(&new_input);
+
+    res = calc(number_input, sign_input);
+    print!("Result: {}", res);
+
     return res;
 }
 
@@ -83,10 +56,9 @@ fn parse_number(input: &String) -> Vec<i32> {
         .replace("-", " ")
         .replace("*", " ")
         .replace("/", " ")
-        .split(" ")
+        .split_whitespace()
         .filter_map(|n| n.parse::<i32>().ok())
         .collect();
-
     println!("{:?}", number);
     return number;
 }
@@ -122,6 +94,9 @@ fn calc(mut number: Vec<i32>, ch: Vec<char>) -> i32 {
 
     let mut res = number[0];
     for a in 0..ch.len() {
+        if a + 1 >= number.len() {
+            break;
+        }
         if ch[a] == '+' {
             res += number[a + 1];
         } else if ch[a] == '-' {
